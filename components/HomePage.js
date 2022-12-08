4c5b50507677521a4702f290e5a7a6210b304c66
KJurkovic9 <karlo.jurkovic11@gmail.com>
2022-12-08 13:02:04 +0100

    a

--- components/HomePage.js
+++ components/HomePage.js
@@ -28,9 +28,7 @@ export default function HomePage({ title, children }) {
           </nav>
         </header>
 
-        <main className="container m-auto mt-4 px-4">
-          {children} Pederi ne zasluzuju zivjet
-        </main>
+        <main className="container m-auto mt-4 px-4">{children}</main>
 
         <footer className="flex h-10 justify-center items-center shadow-inner">
           <p>Copyright &copy; 2022 Victis</p>
