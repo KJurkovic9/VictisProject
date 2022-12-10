b1577e18ec19c9f4b4d56994da16b930a14db55d
KJurkovic9 <karlo.jurkovic11@gmail.com>
2022-12-10 18:39:58 +0100

    products

---
+++ components/ProductItem.js
@@ -0,0 +1,31 @@
+import Link from 'next/link';
+import React from 'react';
+
+export default function ProductItem({ product }) {
+  return (
+    <div className="card">
+      <Link href={`/products/${product.slug}`} legacyBehavior>
+        <a>
+          <img
+            src={product.image}
+            alt={product.name}
+            className="rounded shadow object-fill h-auto w-auto md:h-80 w-72 lg:h-96 w-full"
+          />
+        </a>
+      </Link>
+
+      <div className="flex flex-col items-center justify-center p-5">
+        <Link href={`/products/${product.slug}`} legacyBehavior>
+          <a>
+            <h2 className="text-lg">{product.name}</h2>
+          </a>
+        </Link>
+        <p className="mb-2">{product.brand}</p>
+        <p>${product.price}</p>
+        <button className="primary-button" type="button">
+          Add to cart
+        </button>
+      </div>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,25 @@
 import HomePage from '../components/HomePage';
+import ProductItem from '../components/ProductItem';
+import data from '../utils/data';
 
 export default function Home() {
-  return <HomePage title="Home">Home Page</HomePage>;
+  return (
+    <HomePage title="Home">
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
+        {data.products.map((product) => (
+          <ProductItem product={product} key={product.slug}></ProductItem>
+        ))}
+      </div>
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
+        {data.products.map((product) => (
+          <ProductItem product={product} key={product.slug}></ProductItem>
+        ))}
+      </div>
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
+        {data.products.map((product) => (
+          <ProductItem product={product} key={product.slug}></ProductItem>
+        ))}
+      </div>
+    </HomePage>
+  );
 }
