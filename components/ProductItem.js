348624ce4c3d9111c59158b8bde3ea6b24d0c27c
KJurkovic9 <karlo.jurkovic11@gmail.com>
2023-01-12 23:03:51 +0100

    nova proba

--- components/ProductItem.js
+++ components/ProductItem.js
@@ -1,15 +1,18 @@
 import Link from 'next/link';
 import React from 'react';
+import Image from 'next/image';
 
 export default function ProductItem({ product, addToCartHandler }) {
   return (
     <div className="card">
       <Link href={`/product/${product.slug}`} legacyBehavior>
         <a>
-          <img
+          <Image
             src={product.image}
-            loading="eager"
+            loading="lazy"
             alt={product.name}
+            width="500"
+            height="250"
             className="rounded shadow object-fill h-auto w-full md:h-80 lg:h-52"
           />
         </a>
