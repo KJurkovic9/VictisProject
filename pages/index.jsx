30341bf130e39aff7884dca81a7efafe256e1f58
KJurkovic9 <karlo.jurkovic11@gmail.com>
2023-02-07 19:35:40 +0100

    lets try

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,27 +1,17 @@
 import dynamic from 'next/dynamic';
-import { useContext } from 'react';
-import { toast } from 'react-toastify';
 import Layout from '../components/Layout';
-const Poster = dynamic(() => import('../components/Poster'));
-const ProductCategory = dynamic(() => import('../components/ProductCategory'));
-const Filter = dynamic(() => import('../components/Filter'));
-const SliderShow = dynamic(() => import('../components/Slider'));
+const Poster = dynamic(() => import('../components/Poster'), { ssr: false });
+const ProductCategory = dynamic(() => import('../components/ProductCategory'), {
+  ssr: false,
+});
+const Filter = dynamic(() => import('../components/Filter'), { ssr: false });
+const SliderShow = dynamic(() => import('../components/Slider'), {
+  ssr: false,
+});
 import Product from '../models/Product';
 import db from '../utils/db';
-import { Store } from '../utils/Store';
 
 export default function Home({ products }) {
-  const { state, dispatch } = useContext(Store);
-  const { cart } = state;
-
-  const addToCartHandler = (product) => {
-    const existItem = cart.cartItems.find((x) => x.slug === product.slug);
-    const quantity = existItem ? existItem.quantity + 1 : 1;
-
-    dispatch({ type: 'CART_ADD_ITEM', payload: { ...product, quantity } });
-
-    toast.success('Product added to the cart');
-  };
   return (
     <Layout title="Home">
       <Filter></Filter>
@@ -32,9 +22,10 @@ export default function Home({ products }) {
   );
 }
 
-export async function getServerSideProps() {
+export async function getServerSideProps(context) {
   await db.connect();
   const products = await Product.find().lean();
+
   return {
     props: {
       products: products.map(db.convertDocToObj),
