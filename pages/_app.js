4715079f9a23fe056b31eb89c71bc8679a734d4c
KJurkovic9 <karlo.jurkovic11@gmail.com>
2023-02-14 21:39:14 +0100

    asd

--- pages/_app.js
+++ pages/_app.js
@@ -4,30 +4,8 @@ import { StoreProvider } from '../utils/Store';
 import { useRouter } from 'next/router';
 import { PayPalScriptProvider } from '@paypal/react-paypal-js';
 import { LinearProgress } from '@mui/material';
-import { useState, useEffect } from 'react';
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
-  const [loading, setLoading] = useState(false);
-  const router = useRouter();
-
-  useEffect(() => {
-    const handleStart = (url) => setLoading(true);
-    const handleComplete = (url) => setLoading(false);
-    router.events.on('routeChangeStart', handleStart);
-    router.events.on('routeChangeComplete', handleComplete);
-    router.events.on('routeChangeError', handleComplete);
-
-    return () => {
-      router.events.off('routeChangeStart', handleStart);
-      router.events.off('routeChangeComplete', handleComplete);
-      router.events.off('routeChangeError', handleComplete);
-    };
-  }, [router]);
-
-  if (loading) {
-    return <LinearProgress color="inherit" />;
-  }
-
   return (
     <SessionProvider session={session}>
       <StoreProvider>
